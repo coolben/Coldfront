f60f758719eaea118928fde0f3456a64aaae2b24
Vu Nguyen <vu@Vus-MacBook-Pro.local>
2015-02-28 17:39:08 -0500

    api stubs in controller

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,13 +9,33 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
-  def test = Action {
-      Ok(Json.toJson("GET SUCCESS"))
+    
+  def test(name: String) = Action {
+    Ok(Json.toJson("GET SUCCESS, " + name))
   }
   
   def testPost = Action {
-      Ok(Json.toJson("POST SUCCESS"))
+    Ok(Json.toJson("POST SUCCESS"))
+  }
+  
+  def listPatients = Action{
+    Ok(Json.toJson("list all patients"))
+  }
+  
+  def getPatient(patientId: Long){
+      Ok(Json.toJson("patient id: " + patientId))
+  }
+  
+  def getConditions(patientId: Long){
+      Ok(Json.toJson("return conditions for patient id: " + patientId))
+  }
+  
+  def getMedications(patientId: Long){
+      Ok(Json.toJson("return medications for patient id: " + patientId))
+  }
+  
+  def getObservations(patientId: Long){
+      Ok(Json.toJson("return observations for patient id: " + patientId))
   }
 
 }
