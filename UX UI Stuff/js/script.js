76b3db61055060cec56e8abea9415cdcf0f644ae
cjds <cjds@live.com>
2015-03-21 19:06:51 -0400

    updated UI

--- UX UI Stuff/js/script.js
+++ UX UI Stuff/js/script.js
@@ -3,7 +3,7 @@ var app=angular.module("coldfront",[]);
 app.controller('ToolbarController',  function($scope) {
  	$scope.search=true;
  	$scope.searchfunction=function(){
- 		console.log("SD");
+
 		$scope.search=true;
 	}
 
