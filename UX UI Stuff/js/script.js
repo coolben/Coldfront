0b30669907aa466c0287245904a1d4f0f4a30e5a
Akshar Rawal <akshar.1006@gmail.com>
2015-03-22 02:30:18 -0700

    Static patient data populated and patient names link to patient page displaying MRN. The patient page is to be changed to be a pop-up instead of a separate html page. The data is pretty much static for now.

--- UX UI Stuff/js/script.js
+++ UX UI Stuff/js/script.js
@@ -22,5 +22,54 @@ app.controller('PatientController',  function($scope) {
  	$scope.opensearch=function(){
 		$scope.search=false;
 	}
+
+	$scope.populatePatients = function () {
+		var patients = new Array();
+		patients.push(["Name", "MRN", "DOB", "Primary Complaint", "Attending Physician"]);
+	    patients.push(["Akshar Rawal", "141542", "02/14/1991", "Headache", "Dr. Braunstein"]);
+	    patients.push(["Ben Nguyen", "142324", "05/31/1991", "Stomachache", "Dr. Braunstein"]);
+	    patients.push(["Carl Saldanha", "153234", "12/06/1991", "Sprain", "Dr. Braunstein"]);
+	    patients.push(["Erik Reinerstein", "153466", "01/24/1991", "Burn", "Dr. Braunstein"]);
+	    patients.push(["Vu Nguyen", "153466", "08/10/1991", "Bite", "Dr. Braunstein"]);
+		    
+	    //Create a HTML Table element.
+	    var table = document.createElement("TABLE");
+    	//table.border = "1";
+
+    	columnCount = patients[0].length;
+	    
+	    //Add the header row.
+	    var row = table.insertRow(-1);
+	    for (var i = 0; i < columnCount; i++) {
+	        var headerCell = document.createElement("TH");
+	        headerCell.innerHTML = patients[0][i];
+	        row.appendChild(headerCell);
+	    }
+
+	    //Add the data rows.
+	    for (var i = 1; i < patients.length; i++) {
+	        row = table.insertRow(-1);
+	        for (var j = 0; j < columnCount; j++) {
+	            var cell = row.insertCell(-1);
+	            if (j == 0) {
+	            	cell.innerHTML = "<a href=\"patientPage.html?MRN=" + patients[i][1] + "\"/>" + patients[i][j];	                      
+	            }
+	            else {
+	            	cell.innerHTML = patients[i][j];
+	            }
+	        }
+	    }
+
+	    //Get patient table to populate data
+	    var pTable = document.getElementById("patientTable");
+	    pTable.innerHTML = "";	    
+      	pTable.appendChild(table);
+	}
+
+	$scope.patientDetails = function () {
+		var url = window.location;
+		var detailsPane = document.getElementById("details");
+		detailsPane.innerHTML = (url.search.split('MRN=')[1] ? url.search.split('MRN=')[1] : "No record found.");
+	}
  });
 
